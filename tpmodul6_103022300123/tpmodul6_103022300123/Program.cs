66ec54d3a1bdddc3a696fb050ccf7528efeba4e3
raatra10 <rafi180350@gmail.com>
2025-03-22 14:39:10 +0700

    implementasi design by contract

--- tpmodul6_103022300123/tpmodul6_103022300123/Program.cs
+++ tpmodul6_103022300123/tpmodul6_103022300123/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using System;
+using tpmodul6_103022300123;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – RAFI ATHALLAH RAMADHAN");
+            video.PrintVideoDetails();
+
+            // Tambahkan playCount
+            video.IncreasePlayCount(10000000);
+            video.PrintVideoDetails();
+
+            // menguji batas maksimal integer dengan loop
+            for (int i = 0; i < 250; i++)
+            {
+                video.IncreasePlayCount(10000000); // Akan menyebabkan overflow
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        Console.ReadLine();
+    }
+}
